d37e009919036b522b7d1cf897dbff2427b0b798
LeeHwapyeong <hp0918kr@gmail.com>
2023-10-11 12:07:12 +0900

    2023.10.11 ai연계실습 수업

---
+++ src/main/java/kopo/poly/service/IMovieService.java
@@ -0,0 +1,13 @@
+package kopo.poly.service;
+
+import kopo.poly.dto.MovieDTO;
+
+import java.util.List;
+
+public interface IMovieService {
+
+    int collectMovieRank() throws Exception;
+
+    List<MovieDTO> getMovieInfo() throws Exception;
+
+}
